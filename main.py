22f8bf84d86cddee4e0ab3809c4a488309d12a03
jan-klos <janklos@protonmail.com>
2017-10-16 00:32:40 +0200

    Added tree traversal

--- main.py
+++ main.py
@@ -1,38 +1,39 @@
 import random
 from johnson import johnson
-from tree import create_tree, print_tree, print_nodes_number
+from tree import create_tree, print_tree, print_number_of_nodes, traverse_tree
 
 A1 = [2, 5, 9, 5]
 B2 = [3, 3, 4, 1]
 
-n = 5 # nombre des taches
+n = 4 # nombre des taches
 
 # donnees non-correlees
 A2 = random.sample(range(100), n)
 B2 = random.sample(range(100), n)
+C2 = random.sample(range(100), n)
 
 # correlation sur les durees d'execution
-A3, B3 = [], []
+A3, B3, C3 = [], [], []
 for i in range (0, n):
     r = random.randint(0, 5)
     A3.append(random.randint(r*20, 20 + r*20))
     B3.append(random.randint(r*20, 20 + r*20))
+    C3.append(random.randint(r*20, 20 + r*20))
 
 # correlation sur les machines
 a, b, c = 1, 2, 3
 A4 = [random.randint(15*(a - 1) + 1, 15*(a - 1) + 100) for i in range(0, n)]
 B4 = [random.randint(15*(b - 1) + 1, 15*(b - 1) + 100) for i in range(0, n)]
+C4 = [random.randint(15*(c - 1) + 1, 15*(c - 1) + 100) for i in range(0, n)]
 
-print(A4)
-print(B4)
-
-print(johnson(A4, B4))
+#print(johnson(A4, B4))
 
 # Montrer que l'ordonnancement obtenu est 2-approche pour notre probleme.
 # Preciserer la complexite de cet algo.
 
 
-list = [1, 2, 3, 4]
-tree = create_tree(list)
-print_tree(tree)
-print_nodes_number(tree)
+list = [1, 2, 3]
+tree = create_tree(range(0, n))
+#print_tree(tree)
+print_number_of_nodes(tree)
+traverse_tree(tree, [])
--- tree.py
+++ tree.py
@@ -42,6 +42,20 @@ def create_tree(list):
 
 #################################################################
 
+def traverse_tree(node, current_permutation):
+    if node.children == []:
+        print("Une des possibilites: {list}".format(list = current_permutation))
+        current_permutation.pop(-1)
+        return
+    print(current_permutation)
+    for child in node.children:
+        current_permutation.append(child.value)
+        traverse_tree(child, current_permutation)
+    if current_permutation != []:
+        current_permutation.pop(-1)
+
+#################################################################
+
 def count_nodes(node, n):
     if node.children == []:
         n[0] += 1
@@ -51,14 +65,17 @@ def count_nodes(node, n):
         count_nodes(child, n)
     n[0] += 1
 
-def print_nodes_number(tree):
+#################################################################
+
+def print_number_of_nodes(tree):
     n = [-1, 0] # tableau parce qu'il est transmis par reference (int par affectation). -1 pour que la racine ne soit pas prise en compte
     count_nodes(tree, n)
     print("Nombre de noeuds (feuilles incluses, racine exclue): {0}. Nombre de feuilles: {1}".format(n[0], n[1]))
 
+#################################################################
+
 def print_tree(node):
     if node.children == []:
-        #print(node.value, end = " ")
         return
     node.print_children()
     for child in node.children:
